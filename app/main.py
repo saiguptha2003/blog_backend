c82f079369625e846e35f655a0435c55b27ec55b
Sai Guptha CM <sai.guptha@cm.com>
2025-05-29 15:07:24 +0530

    Add startup and shutdown event logging to FastAPI application

--- app/main.py
+++ app/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
 from app.routers import users, blogs
 from app.database import Base, engine
+from app.utils.logger import logger
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+@app.on_event("startup")
+async def startup_event():
+    logger.info("Starting up BlogApp")
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    logger.info("Shutting down BlogApp")
 app.include_router(users.router)
 app.include_router(blogs.router)
